d48aac733191f5ceaa34bab828adcad820f96ce8
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-23 11:26:42 -0600

    Add styling to side bar and category weights

--- src/components/ShelfStatistics/ShelfStatistics.js
+++ src/components/ShelfStatistics/ShelfStatistics.js
@@ -2,15 +2,23 @@ import React from "react";
 
 const ShelfStatistics = ({ shelves }) => {
   const shelfWeights = shelves.map((shelf, i) => {
-    const shelfWeightInfo = Object.entries(shelf)
+    const shelfWeightInfo = Object.entries(shelf).flat()
     return (
-      <li key={i}>{shelfWeightInfo[0]}: {shelfWeightInfo[1]}</li>
+      <li 
+        key={i}
+        className="statistics-category"
+      >
+          <span className="statistics-category-title">{shelfWeightInfo[0]}:</span>{shelfWeightInfo[1]} 
+      </li>
     )
   })
   return (
-    <ul>
-      {shelfWeights}
-    </ul>
+    <div className="statistics-category-container">
+      <h2>The Breakdown</h2>
+      <ul className="statistics-category-list">
+        {shelfWeights}
+      </ul>
+    </div>
   )
 }
 
