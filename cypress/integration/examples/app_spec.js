b6c493ccdbcc9040216338a29c98d0df089560ae
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-26 10:55:47 -0600

    Clean up file formatting

--- cypress/integration/examples/app_spec.js
+++ cypress/integration/examples/app_spec.js
@@ -4,7 +4,7 @@ describe ("Landing Page", () => {
     cy.visit("http://localhost:3000/");
   })
   it("should display a user form to access dashboard", () => {
-    cy.get("[data-cy=user-portal]").should("contain", "Gearfull")
+    cy.get("[data-cy=user-portal]").should("contain", "Gearfull");
     cy.get("[data-cy=user-portal-input]").type("Elizabeth");
     cy.get("[data-cy=user-portal-btn]").click();
     cy.url().should('include', '/dashboard');
@@ -14,8 +14,8 @@ describe ("Landing Page", () => {
 describe("User Dashboard", () => {
   beforeEach(() => {
     
-    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"})
-    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"})
+    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"});
+    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
     cy.visit("http://localhost:3000/")
     cy.get("[data-cy=user-portal-input]").type("Elizabeth");
     cy.get("[data-cy=user-portal-btn]").click();
@@ -24,7 +24,7 @@ describe("User Dashboard", () => {
   it("should display a user's name and main dashboard features upon load", () => {
     cy.get("[data-cy=dashboard-title]").contains("Gearfull");
     cy.get("[data-cy=greeting]").contains("Welcome Elizabeth");
-    cy.get("[data-cy=shelves-intro]").contains('Create some shelves here like "navigation" or "cook system"...')
+    cy.get("[data-cy=shelves-intro]").contains('Create some shelves here like "navigation" or "cook system"...');
     cy.get("[data-cy=add-shelf-form]").should("exist");
     cy.get("[data-cy=add-shelf-btn]").contains("add a shelf");
     cy.get("[data-cy=statistics-box]").should("contain", "40.00 Oz")
@@ -40,7 +40,7 @@ describe("User Dashboard", () => {
     .and("contain", "amount: 1");
     cy.get("[data-cy=pack-weight-oz]").contains("40.00");
     cy.get("[data-cy=pack-weight-lbs]").contains("2.50 Lbs");
-    cy.get("[data-cy=shelf-weight-name]").should("contain", "navigation")
+    cy.get("[data-cy=shelf-weight-name]").should("contain", "navigation");
     cy.get("[data-cy=shelf-weight-oz]").contains("40.00 Oz");
     cy.get("[data-cy=shelf-weight-lb]").contains("2.50 Lbs");
   });
@@ -48,23 +48,23 @@ describe("User Dashboard", () => {
 
 describe("Loading messages", () => {
   it("should show an error message if shelves can't be loaded", () => {
-    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"})
+    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"});
     cy.visit("http://localhost:3000/dashboard");
     cy.get("[data-cy=loading-error-msg]").contains("We can't load your shelves right now, please try again later");
   });
 
   it("should show a loading message if shelves are loading", () => {
-    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"})
+    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
     cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"});
     cy.visit("http://localhost:3000/dashboard");
-    cy.contains('Loading shelves...')
+    cy.contains('Loading shelves...');
 
   });
 
   it("should show a message on load when shelves are empty", () => {
     cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves_empty.json"});
     cy.visit("http://localhost:3000/dashboard");
-    cy.contains("Your shelves are empty")
+    cy.contains("Your shelves are empty");
 
   })
 })
@@ -72,9 +72,9 @@ describe("Loading messages", () => {
 describe("Adding an item", () => {
   beforeEach(() => {
 
-    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"})
+    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
     cy.intercept("PUT", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation", {fixture: "item2.json"});
-    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"})  
+    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"}); 
     cy.visit("http://localhost:3000/dashboard");
 
   });
@@ -90,8 +90,8 @@ describe("Adding an item", () => {
     .and("contain", "amount: 1");
     cy.get("[data-cy=statistics-box]").should("contain", "74.60 Oz")
     .and("contain", "4.66 Lbs");
-    cy.get("[data-cy=shelf-weight-oz]").first().contains("74.60 Oz")
-    cy.get("[data-cy=shelf-weight-lb]").first().contains("4.66 Lbs")
+    cy.get("[data-cy=shelf-weight-oz]").first().contains("74.60 Oz");
+    cy.get("[data-cy=shelf-weight-lb]").first().contains("4.66 Lbs");
   });
 
   it("should give the user an error message if any form fields are empty", () => {
@@ -129,9 +129,9 @@ describe("Adding an item", () => {
 describe("Removing an item", () => {
   beforeEach(() => {
 
-    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"})
-    cy.intercept("POST", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation", {fixture: "item3.json"})
-    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"})  
+    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
+    cy.intercept("POST", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation", {fixture: "item3.json"});
+    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"});  
     cy.visit("http://localhost:3000/dashboard");
   });
 
@@ -143,17 +143,17 @@ describe("Removing an item", () => {
     cy.get("[data-cy=added-item]").should("not.exist");
     cy.get("[data-cy=statistics-box]").should("contain", "0.00 Oz")
     .and("contain", "0.00 Lbs");
-    cy.get("[data-cy=shelf-weight-oz]").first().contains("0.00 Oz")
-    cy.get("[data-cy=shelf-weight-lb]").first().contains("0.00 Lbs")
+    cy.get("[data-cy=shelf-weight-oz]").first().contains("0.00 Oz");
+    cy.get("[data-cy=shelf-weight-lb]").first().contains("0.00 Lbs");
   });
 });
 
 describe("Adding a shelf", () => {
   beforeEach(() => {
 
-    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"})
-    cy.intercept("POST", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: "item3.json"})
-    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"})  
+    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
+    cy.intercept("POST", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: "item3.json"});
+    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"}); 
     cy.visit("http://localhost:3000/dashboard");
 
   });
@@ -176,8 +176,8 @@ describe("Deleting a shelf", () => {
   beforeEach(() => {
    
     cy.intercept("DELETE", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: ""});
-    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"})
-    cy.intercept("POST", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: "item3.json"})
+    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
+    cy.intercept("POST", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: "item3.json"});
     cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"})  
     cy.visit("http://localhost:3000/dashboard");
   });
@@ -189,7 +189,7 @@ describe("Deleting a shelf", () => {
     cy.get("[data-cy=modal-warning-msg-1]").contains("Are you sure you want to remove this shelf?");
     cy.get("[data-cy=modal-warning-msg-2]").contains("This will delete the shelf and all it's contents");
     cy.get("[data-cy=modal-remove-btn]").contains("Yes remove please");
-    cy.get("[data-cy=modal-return-btn]").contains("No take me back")
+    cy.get("[data-cy=modal-return-btn]").contains("No take me back");
   
   }); 
 
@@ -209,20 +209,19 @@ describe("Deleting a shelf", () => {
     cy.get("[data-cy=shelf]").eq(1).should("not.exist"); 
     cy.get("[data-cy=shelves]").should("contain", "navigation")
       .and("not.contain", "cooking"); 
-
   });
 
   it("Will adjust pack weight if deleted shelf contained items", () => {
     cy.get("[data-cy=remove-category]").eq(0).click();
     cy.get("[data-cy=modal-remove-btn]").click();
     cy.get("[data-cy=pack-weight-oz]").should("contain", "0.00 Oz");
-    cy.get("[data-cy=pack-weight-lbs]").should("contain", "0.00 Lbs")
+    cy.get("[data-cy=pack-weight-lbs]").should("contain", "0.00 Lbs");
     cy.get("[data-cy=statistics-box]").should("not.contain", "navigation"); 
   });
 
-  it.only("should show an empty shelf message when all shelves are delted", () => {
+  it("should show an empty shelf message when all shelves are delted", () => {
     cy.get("[data-cy=remove-category]").eq(0).click();
     cy.get("[data-cy=modal-remove-btn]").click();
     cy.get("[data-cy=shelves-msg]").contains("Your shelves are empty");
-  })
+  });
 });
