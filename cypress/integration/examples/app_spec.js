c07da3e3df9a8a86e10b2e6a8358796877fe4722
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-26 12:26:21 -0600

    Fix test stub for server error message

--- cypress/integration/examples/app_spec.js
+++ cypress/integration/examples/app_spec.js
@@ -51,20 +51,32 @@ describe("User Dashboard", () => {
     cy.url().should('include', '/');
     cy.get("[data-cy=user-portal]").should("contain", "Gearfull");
 
-  })
+  });
 });
 
-describe("Loading messages", () => {
+describe.only("Loading messages", () => {
   it("should show an error message if shelves can't be loaded", () => {
-    cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"});
+
+    cy.intercept({
+      method: 'GET',
+      url: 'https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation'
+    },
+    {
+      statusCode: 500,
+      body: { 
+        message: `Shelves can't be loaded at this time` 
+      }
+    })
+    cy.intercept("https://getpantry.cloud/apiv1/pantry/shelferror", {fixture:"shelves.json"});
     cy.visit("http://localhost:3000/dashboard");
     cy.get("[data-cy=loading-error-msg]").contains("We can't load your shelves right now, please try again later");
   });
 
   it("should show a loading message if shelves are loading", () => {
-    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
+
     cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"});
-    cy.visit("http://localhost:3000/dashboard");
+    cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
+    cy.visit("http://localhost:3000/dashboard")
     cy.contains('Loading shelves...');
 
   });
@@ -74,8 +86,8 @@ describe("Loading messages", () => {
     cy.visit("http://localhost:3000/dashboard");
     cy.contains("Your shelves are empty");
 
-  })
-})
+  });
+});
 
 describe("Adding an item", () => {
   beforeEach(() => {
