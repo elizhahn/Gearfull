e72036c7edb62c19edc7f175d7d099696d171f9a
Elizabeth Hahn <elizhahn87@gmail.com>
2021-05-03 20:20:44 -0600

    Add extra testing to delete shelf to prevent it from asserting too soon

--- cypress/integration/examples/app_spec.js
+++ cypress/integration/examples/app_spec.js
@@ -206,7 +206,7 @@ describe("Adding a shelf", () => {
 describe("Deleting a shelf", () => {
   beforeEach(() => {
    
-    cy.intercept("DELETE", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: ""})
+    cy.intercept("DELETE", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: ""}).as("deleteShelf")
     cy.intercept(`https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/navigation`, {fixture: "item1.json"});
     cy.intercept("POST", "https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d/basket/cooking", {fixture: "item3.json"});
     cy.intercept("https://getpantry.cloud/apiv1/pantry/929de230-c666-4f11-9602-b7c818abee8d", {fixture:"shelves.json"})  
@@ -232,11 +232,15 @@ describe("Deleting a shelf", () => {
     cy.get("[data-cy=modal]").should("not.exist"); 
   });
 
-  it("Allows a user to delete a shelf after being warned", () => {
+  it.only("Allows a user to delete a shelf after being warned", () => {
     cy.get("[data-cy=add-shelf-input]").type("cooking");
     cy.get("[data-cy=add-shelf-btn]").click();
     cy.get("[data-cy=remove-category]").eq(0).click();
     cy.get("[data-cy=modal-remove-btn]").click();
+    cy.wait("@deleteShelf").then((response) => {
+      expect(response.request.body).to.contain("")
+    })
+    cy.get("[data-cy=modal]").should("not.exist"); 
     cy.get("[data-cy=shelf]").eq(1).should("not.exist"); 
     cy.get("[data-cy=shelves]").should("contain", "navigation")
       .and("not.contain", "cooking"); 
